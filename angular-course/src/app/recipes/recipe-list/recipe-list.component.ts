ffbab367dd5675bb201a9a83abfa78c4a613e5bf
Dudeplzs <74299433+Dudeplzs@users.noreply.github.com>
2020-12-05 13:23:01 +0000

    Section 3 - Chapter 54

--- angular-course/src/app/recipes/recipe-list/recipe-list.component.ts
+++ angular-course/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,3 +1,4 @@
+import { Recipe } from '../recipes.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe [] = [
+    new Recipe('Lasanha', 'Lasanha de Carne', 'https://www.clubedereceita.com.br/wp-content/uploads/2019/05/receita-de-lasanha-rapida.jpg'),
+    new Recipe('Carbonara', 'Carbonara com natas', 'https://www.nit.pt/wp-content/uploads/2018/02/9da364f17ac13d1fc3235a0314890a5d.jpg')
+  ];
   constructor() { }
 
   ngOnInit(): void {
