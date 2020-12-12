36b8c21eb0eb4207aadb9ccf1f57a60c6d8943c6
Familia <rodrigo_falc@hotmail.com>
2020-12-12 01:51:34 +0000

    Section 5: Chapter 64

---
+++ angular-course/src/app/shared/ingredient.mode.ts
@@ -0,0 +1,14 @@
+export class Ingredient{
+    /*
+    public name: string;
+    public amount: number;
+    */
+    // Fazer isto substitui o que se encontra comentado!!
+    constructor(public name: string, public amount: number){
+    /*
+    constructor(name: string, amount: number)
+    this.name=name;
+    this.amount=amount;
+    */
+    }
+}
--- angular-course/src/app/shopping-list/shopping-list.component.ts
+++ angular-course/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.mode';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 6),
+    new Ingredient('Potatos', 10)
+
+  ];
 
   constructor() { }
 
