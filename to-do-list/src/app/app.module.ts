25c12eef89c4c0b3c451ab620f21f957962e0f06
Familia <rodrigo_falc@hotmail.com>
2021-01-27 10:35:44 +0000

    Adding to Repository

--- to-do-list/src/app/app.module.ts
+++ to-do-list/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { ListComponent } from './list/list.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [LogService],
   bootstrap: [AppComponent]
--- to-do-list/src/app/list/list.component.ts
+++ to-do-list/src/app/list/list.component.ts
@@ -1,3 +1,4 @@
+import { ServiceSharedService } from './../service-shared.service';
 import { LogService } from './../logg.service';
 import { Component, OnInit } from '@angular/core';
 
@@ -7,43 +8,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
-  lista: ListElement [] = [];
+  // lista: ListElement [] = [];
   // checkBoxStatus = false;
+  listaElement: any = [];
 
-  constructor(private logservice: LogService) { }
+  constructor(private logservice: LogService,
+              private serviceShare: ServiceSharedService) { }
 
   // tslint:disable-next-line: typedef
-  ngOnInit() {
+  ngOnInit(): void {
+    this.refreshLista();
   }
 
   // tslint:disable-next-line: typedef
-  onGo(nameInput: HTMLInputElement){
-    const listElement: ListElement = {text: nameInput.value, status: false};
-    this.lista.push(listElement);
-    console.log(this.lista);
+  refreshLista(){
+    this.serviceShare.getLista().subscribe(
+      data => { this.listaElement = data;
+      });
   }
 
   // tslint:disable-next-line: typedef
-  onClear(){
-    this.lista = [];
-  }
+  // onGo(nameInput: HTMLInputElement){
+  //   const listElement: ListElement = {text: nameInput.value, status: false};
+  //   this.lista.push(listElement);
+  //   console.log(this.lista);
+  // }
 
   // tslint:disable-next-line: typedef
-  onRemoveItem(index: number){
-    this.lista.splice(index, 1);
-  }
+  // onClear(){
+  //   this.listaElement = [];
+  // }
 
   // tslint:disable-next-line: typedef
-  onCheckboxClick(element: ListElement){
-    element.status = !element.status;
+  // onRemoveItem(index: number){
+  //   this.lista.splice(index, 1);
+  // }
+
+  // tslint:disable-next-line: typedef
+  onCheckboxClick(element: any){
+    element.Estado = !element.Estado;
+    // var var = {}
+    this.serviceShare.updateLista(element);
     // this.lista[index];
-    this.logservice.logChange(element.text, element.status);
+    // this.logservice.logChange(element.text, element.status);
+    console.log();
   }
 
 
 }
 
-export interface ListElement{
-  text: string;
-  status: boolean;
-}
+// export interface ListElement{
+//   id: number;
+//   text: string;
+//   status: boolean;
+// }
